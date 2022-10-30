eb20715637ecbec930ae43d701de967c178cab35
Brian Kropaczek <kropaczekb@gmail.com>
2022-10-30 14:32:28 -0400

    Firebase with svelte functionality

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: '/app/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.svelte$/,
+        use: {
+            loader: 'svelte-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        use: 'file-loader',
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.mjs', '.svelte'],
+  },
+  plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            inject: false,
+        }),
+        new MiniCssExtractPlugin(),
+        new Dotenv(),
+    ],
+}
