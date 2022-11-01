cf21694f83e96763ff3609b3997f258ef33ef91c
Brian Kropaczek <kropaczekb@gmail.com>
2022-11-01 14:14:15 -0400

    Dimensions stored in db

--- app/routes/PixelArt.svelte
+++ app/routes/PixelArt.svelte
@@ -4,36 +4,45 @@
   import app from '../../src/index'
   import { getAuth, onAuthStateChanged } from 'firebase/auth';
   import { onMount } from 'svelte';
-  let height = 10;
-  let width = 10;
+
+  let rerenderValue = true;
   let selectedColor = "red";
-  // let listOfColors = new Array(5).fill(new Array(5).fill('grey'));
-  let listOfColors = new Array(height)
-  for (let i = 0; i < listOfColors.length; i++) {
-    listOfColors[i] = new Array(width).fill('grey');
-  }
-  let grid = new Array(height)
-  for (let i = 0; i < grid.length; i++) {
-    grid[i] = new Array(width)
-  }
+  let listOfColors;
+  let height;
+  let width;
   const database = getDatabase();
   const colors = ref(database, "colors/");
+  const sizes = ref(database, "dimensions/");
 
-  onValue(colors, (snapshot) => {
-    let data = snapshot.val();
-    listOfColors = Object.values(data)[0]
+  onValue(sizes, async (snapshot) => {
+    let data = await snapshot.val();
+    height = data.height
+    width = data.width
+    height = height
+    width = width
+  })
+
+  onValue(colors, async (snapshot) => {
+    let data = await snapshot.val();
+    listOfColors = await Object.values(data)[0]
+    listOfColors = listOfColors
+  })
+
+  onMount(async () => {
   })
 
   async function click(event) {
-
+    console.log("listofcolors", listOfColors)
     let column = event.srcElement.cellIndex;
     let row = event.srcElement.parentNode.rowIndex;
-
     listOfColors.length = height;
     for (let i = 0; i < listOfColors.length; i++){
+      if (listOfColors[i] === undefined) {
+        listOfColors[i] = new Array(width)
+      }
       listOfColors[i].length = width;
       for (let j = 0; j < listOfColors[i].length; j++) {
-        if (listOfColors[i][j] === undefined) {
+        if (!listOfColors[i][j]) {
         listOfColors[i][j] = 'grey'
       }}
     }
@@ -57,20 +66,33 @@
   <option value="white">White</option>
 </select>
 
+
+<input type:number bind:value={height} min=1 max=30>
+<input type:number bind:value={width} min=1 max=30>
+
+{#if listOfColors}
 <table>
-    {#each grid as row, rowindex}
+    {#each listOfColors as row, rowindex}
       <tr id="row-{rowindex}">
         {#each row as square, cellindex}
           {#if listOfColors.length - 1 >= rowindex}
+
+            <!-- {#if listOfColors[rowindex].length - 1 <= cellindex} -->
             <td id="cell-{cellindex}" on:click="{(event) => click(event)}" class={listOfColors[rowindex][cellindex]}></td>
+            <!-- {:else}
+              <td id="cell-{cellindex}" on:click="{(event) => click(event)}" class={'grey'}></td>
+            {/if} -->
+
           {:else}
             <td id="cell-{cellindex}" on:click="{(event) => click(event)}" class={'grey'}></td>
           {/if}
-
         {/each}
       </tr>
     {/each}
-</table>
+  </table>
+  {:else}
+<p>Loading...</p>
+  {/if}
 
 <style>
   :global(.red) {
